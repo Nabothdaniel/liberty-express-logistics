a9045a769a0110a3230b700b7e34e3d39c8457cb
Nabothdaniel <nabothdaniel44@gmail.com>
2025-07-04 01:05:40 +0100

    a

--- src/components/landingPage/Header.jsx
+++ src/components/landingPage/Header.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import { Link } from "react-router-dom";
 export default function Header() {
     const [menuOpen, setMenuOpen] = useState(false);
 
